75d3b0820543f8c41aa91bacab8ebcc67fdaa90f
Miguel Nazario <mnazario35@gmail.com>
2023-06-08 10:39:35 -0400

    debugging slider for phone view, second try

--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -10,7 +10,7 @@ function Slide({ item }) {
                     component="img"
                     alt={item.title}
                     image={item.image}
-                    // sx={{ maxWidth: 50 }}
+                    sx={{ minHeight: 250 }}
                 />
             </Card>
             <Stack direction="row" spacing={2} m={3} justifyContent="center" alignItems="center" divider={<Divider orientation="vertical" flexItem />} >
