f122588ce739146464e40334661ff0375c7bc641
Miguel Nazario <mnazario35@gmail.com>
2023-06-16 19:47:40 -0400

    fixed carousel responsiveness

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,9 +3,15 @@ import Carousel from 'react-material-ui-carousel';
 import Slide from "./Slide";
 import {projects} from "../Helper/projects";
 
+const carousel = {
+    maxWidth: 1000,
+    height: { xs: 505, sm: "fit-content" },
+    margin: 'auto'
+};
+
 function Projects() {
     return (
-        <Carousel sx={{ maxWidth: 1000, height: "fit-content", margin: 'auto' }}>
+        <Carousel sx={ carousel }>
             {
                 projects.map( (item) => <Slide key={item.id} item={item} /> )
             }
